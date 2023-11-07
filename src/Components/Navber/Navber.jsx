7301de7cbd78c1c7c3cd666baad3b13f72740221
Abdullah's PC <abdullahalrahman23@gmail.com>
2023-11-07 23:37:05 +0300

    fixed login page

--- src/Components/Navber/Navber.jsx
+++ src/Components/Navber/Navber.jsx
@@ -15,11 +15,15 @@ const Navber = () => {
         <li><NavLink className='no-underline' to={'/mypostedjob'}>My Posted Jobs</NavLink></li>
         <li><NavLink className='no-underline' to={'/bidsrequest'}>Bids Request</NavLink></li>
         <li><NavLink className='no-underline' to={'/mybids'}>My Bids</NavLink></li>
+        {
+            user ? "" : <> <li> <NavLink className='no-underline' to='/login'>login</NavLink></li>
+                <li> <NavLink className='no-underline' to='/register'>Register</NavLink></li></>
+        }
     </>
     return (
-        <div className="navbar flex justify-between items-center bg-base-100">
+        <div className="navbar flex items-center justify-between bg-base-100">
             <div className="">
-                <div className="dropdown block  md:hidden lg:hidden">
+                <div className="dropdown block lg:hidden">
                     <label tabIndex={0} className="btn btn-ghost lg:hidden">
                         <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h8m-8 6h16" /></svg>
                     </label>
@@ -28,46 +32,24 @@ const Navber = () => {
                     </ul>
                 </div>
                 <div className="flex items-center">
-                    <img className="w-[30px] h-[30px]" src={iconjob} alt="" />
-                    <a className="btn btn-ghost normal-case text-xl">JobSeeker</a>
-
+                    <img src={iconjob} className='w-[30px]' alt="" />
+                    <a className="btn btn-ghost normal-case text-xl">jobSeeker</a>
                 </div>
             </div>
-            <div className="hidden lg:flex">
+            <div className=" hidden lg:flex">
                 <ul className="menu menu-horizontal px-1">
                     {navlinks}
                 </ul>
             </div>
             <div className="">
                 {
-                    user ?
-                        <div className="flex">
-                            <div className="flex flex-col items-center mr-2">
-                                <p className="outline-1 rounded-lg text-xs hidden md:block lg:block">{user.displayName ? user.displayName : user.email}</p>
-                                <div className="block md:flex lg:flex items-center">
-                                    <Link><button onClick={handleUser} className="btn btn-xs">Logout</button></Link>
-                                    {/* <input type="checkbox" className="toggle] h-[10px" checked /> */}
-                                    <div>
+                    user ? <><button>logout</button></> :
+                        <>
 
-                                    </div>
-
-                                </div>
-                            </div>
-                            <div className="avatar">
-                                <div className="w-10 rounded-full">
-                                    <img src={user.photoURL && user.photoURL} />
-                                </div>
-                            </div>
-                        </div>
-                        :
-                        <div className="flex justify-between">
-                            <NavLink className='mr-2 no-underline' to={'/login'}>Login</NavLink>
-                            <NavLink className='no-underline' to={'/register'}>Register</NavLink>
-                        </div>
-
+                        </>
                 }
             </div>
-        </div>
+        </div >
     );
 };
 
--- src/Pages/Login/Login.jsx
+++ src/Pages/Login/Login.jsx
@@ -2,6 +2,7 @@ import { AuthContext } from "../../AuthProvider/AuthProvider";
 import { useContext, useState } from "react";
 import { Link, useNavigate } from "react-router-dom";
 import { ToastContainer, toast } from 'react-toastify';
+import loginImage from '../../assets/sign-in-page-abstract-concept-vector-illustration-enter-application-mobile-screen-user-login-form-website-page-interface-ui-new-profile-registra-2J3T8FG-removebg-preview.png'
 const Login = () => {
     const [loginMes, setLoginMes] = useState('')
     const userInfo = useContext(AuthContext)
@@ -44,34 +45,36 @@ const Login = () => {
 
     }
     return (
-        <div className="hero min-h-screen bg-base-200">
-            <div className="hero-content flex-col ">
-                <div className="text-center ">
-                    <h1 className="text-5xl font-bold">Login now!</h1>
-                </div>
-                <div className="card flex-shrink-0 w-full max-w-sm shadow-2xl bg-base-100">
-                    <form onSubmit={handleSignIn} className="card-body">
-                        <div className="form-control">
-                            <label className="label">
-                                <span className="label-text">Email</span>
-                            </label>
-                            <input type="email" name="email" placeholder="email" className="input input-bordered" required />
-                        </div>
-                        <div className="form-control">
-                            <label className="label">
-                                <span className="label-text">Password</span>
-                            </label>
-                            <input type="password" name="password" placeholder="password" className="input input-bordered" required />
-                            <label className="label">
-                                <a href="#" className="label-text-alt link link-hover">Forgot password?</a>
-                            </label>
-                        </div>
-                        <div className="form-control mt-6">
-                            <button className="btn btn-primary">Login</button>
-                        </div>
-                    </form>
-                    <button onClick={handlePopUp}>Login via Google</button>
-                    <p className="p-5">Do not have a account?<Link to={'/register'} className="text-blue-500">Register</Link></p>
+        <div className="flex items-center justify-center bg-slate-200">
+            <div className="hidden md:block lg:block">
+                <img className="w-[1000px]" src={loginImage} alt="" />
+            </div>
+            <div className="hero">
+                <div className="hero-content flex-col ">
+                    <div className="card flex-shrink-0 w-full  shadow-2xl bg-base-100">
+                        <form onSubmit={handleSignIn} className="card-body">
+                            <div className="form-control">
+                                <label className="label">
+                                    <span className="label-text">Email</span>
+                                </label>
+                                <input type="email" name="email" placeholder="email" className="input input-bordered" required />
+                            </div>
+                            <div className="form-control">
+                                <label className="label">
+                                    <span className="label-text">Password</span>
+                                </label>
+                                <input type="password" name="password" placeholder="password" className="input input-bordered" required />
+                                <label className="label">
+                                    <a href="#" className="label-text-alt link link-hover">Forgot password?</a>
+                                </label>
+                            </div>
+                            <div className="form-control mt-6 text-center">
+                                <button className="btn btn-primary w-full ">Login</button>
+                            </div>
+                        </form>
+                        <button onClick={handlePopUp}>Login via Google</button>
+                        <p className="p-5">Do not have a account?<Link to={'/register'} className="text-blue-500">Register</Link></p>
+                    </div>
                 </div>
             </div>
         </div>
