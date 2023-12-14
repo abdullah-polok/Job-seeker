ed6915b41ffacf9e8162f95cd4b7ab9b0a3900c2
Abdullah's PC <abdullahalrahman23@gmail.com>
2023-12-14 21:22:45 +0300

    embaded google map

--- src/Features/Fetaure.jsx
+++ src/Features/Fetaure.jsx
@@ -1,22 +1,73 @@
 import ChatBot from 'react-simple-chatbot';
+import { FaRegMessage } from "react-icons/fa6";
 const Fetaure = () => {
-
-
     const steps = [
         {
-            id: '0',
-            message: 'Welcome to react chatbot!',
-            trigger: '1',
+            id: "Greet",
+            message: "Hello, Welcome to our shop",
+            trigger: "Done",
         },
         {
-            id: '1',
-            message: 'Bye!',
+            id: "Done",
+            message: "Please enter your name!",
+            trigger: "waiting1",
+        },
+        {
+            id: "waiting1",
+            user: true,
+            trigger: "Name",
+        },
+        {
+            id: "Name",
+            message: "Hi {previousValue}, Please select your issue",
+            trigger: "issues",
+        },
+        {
+            id: "issues",
+            options: [
+                {
+                    value: "React",
+                    label: "React",
+                    trigger: "React",
+                },
+                { value: "Angular", label: "Angular", trigger: "Angular" },
+            ],
+        },
+        {
+            id: "React",
+            message:
+                "Thanks for letting your React issue, Our team will resolve your issue ASAP",
+            end: true,
+        },
+        {
+            id: "Angular",
+            message:
+                "Thanks for letting your Angular issue, Our team will resolve your issue ASAP",
             end: true,
         },
     ];
+
     return (
-        <div>
-            <ChatBot steps={steps} />
+        <div className=''>
+
+            <div className='avatar'>
+                <button className=" rounded-full shadow-lg shadow-gray-300" onClick={() => document.getElementById('my_modal_3').showModal()}><FaRegMessage className='bg-[#6e48aa] text-white w-12 h-12 p-3 rounded-full'></FaRegMessage></button>
+            </div>
+            <dialog id="my_modal_3" className="modal">
+                <div className="modal-box">
+                    <form method="dialog">
+                        {/* if there is a button in form, it will close the modal */}
+                        <button className="btn btn-sm btn-circle btn-ghost absolute right-2 top-2">✕</button>
+                    </form>
+                    <div className="w-full">
+                        <ChatBot
+
+                            steps={steps}
+                        />
+
+                    </div>
+                </div>
+            </dialog>
         </div>
     );
 };
--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -3,6 +3,7 @@ import Banner from "../../Components/Banner/Banner";
 import JobCategory from "../../Components/Job Category/JobCategory";
 import ShowImage from "../../Components/showimage/ShowImage";
 import State from "../../Components/State/State";
+import Fetaure from "../../Features/Fetaure";
 
 const Home = () => {
     return (
@@ -10,6 +11,7 @@ const Home = () => {
             <Banner></Banner>
             <JobCategory></JobCategory>
             <ShowImage></ShowImage>
+            <Fetaure></Fetaure>
             <State></State>
 
         </div>
